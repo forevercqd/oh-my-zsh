ab7a23c05cee86eeb64138930d8d65421a99e587
robert <robert@robert-ubuntu16.04>
2017-09-26 14:44:09 +0800

    1. 增加插件修改

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -4,3 +4,6 @@ zsh --version
 sudo chsh -s $(which zsh)
 echo $SHELL
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# 安装 oh-my-zsh 插件
+sudo vim ~/.zshrc   # 可在此文件中搜索 plugins, 对应行修改成 plugins=(git extract z sublime)

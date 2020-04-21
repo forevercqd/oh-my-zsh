9e4ab946141a24386ff7e31f4efe91c05c436038
Robert <forevercqd@163.com>
2020-04-21 23:14:31 +0800

    fix: 修改Mac平台编译问题;

--- oh-my-zsh-install.sh
+++ oh-my-zsh-install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-sudo apt-get install zsh
+#sudo apt-get install zsh
+brew install zsh
 zsh --version
-sudo chsh -s $(which zsh)
+chsh -s $(which zsh)
 echo $SHELL
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 # 安装 oh-my-zsh 插件
-sudo vim ~/.zshrc   # 可在此文件中搜索 plugins, 对应行修改成 plugins=(git extract z sublime)
+vim ~/.zshrc   # 可在此文件中搜索 plugins, 对应行修改成 plugins=(git extract z sublime)
